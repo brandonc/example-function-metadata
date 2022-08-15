da965478140b10a8a36f1daea7462dacb4789f34
Brandon Croft <brandon.croft@gmail.com>
2022-08-15 15:05:44 -0600

    prec

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
-# locals {
-#   a = join(["foo"], "bar")
-#   b = max([1, 2, 3])
-#   c = abs("minus five")
-#   d = jsondecode("[1, 2, 3")
-# }
+locals {
+  # a = join(["foo"], "bar")
+  # b = max([1, 2, 3])
+  # c = abs("minus five")
+  # d = jsondecode("[1, 2, 3")
+  e = 101
+}
 
 resource "random_pet" "dog" {
   lifecycle {
-    postcondition {
-      condition     = self.length == 100
+    precondition {
+      condition     = local.e == 100
       error_message = "length must be exactly 100"
     }
   }
